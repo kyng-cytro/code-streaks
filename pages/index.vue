0f2032bf3f14cdee63bbb3ae7d4133715f287303
kyng-cytro <johndibashi2001@gmail.com>
2022-11-27 23:10:48 +0100

    :bug: now works with localserver but still making too many calls

--- pages/index.vue
+++ pages/index.vue
@@ -2,32 +2,69 @@
   <Head>
     <Title>Code Streaks | Today</Title>
   </Head>
-  <div
-    class="mt-6 flex flex-col items-center justify-center space-y-10"
-    v-if="data"
-  >
-    <!-- Page Header -->
-    <div class="flex flex-col items-center space-y-4">
-      <h1 class="text-2xl font-semibold">Today's Challenge</h1>
-      <div class="flex flex-col items-center space-y-2 text-sm">
-        <div class="flex gap-6">
-          <p>Difficulty: {{ data.difficulty }}</p>
-          <p>Category: {{ data.category }}</p>
-        </div>
-        <div>
-          <p>
-            Tags:
-            <span class="ml-1" v-for="tag in data.tags">{{ tag.name }}</span>
-          </p>
+  <ClientOnly>
+    <div
+      class="mt-6 flex flex-col items-center justify-center space-y-10"
+      v-if="question"
+    >
+      <!-- Page Header -->
+      <div class="flex flex-col items-center space-y-4">
+        <h1 class="text-2xl font-semibold">Today's Challenge</h1>
+        <div class="flex flex-col items-center space-y-2 text-sm">
+          <div class="flex gap-6">
+            <p>Difficulty: {{ question.difficulty }}</p>
+            <p>Category: {{ question.category }}</p>
+          </div>
+          <div>
+            <p>
+              Tags:
+              <span class="ml-1" v-for="tag in question.tags">{{
+                tag.name
+              }}</span>
+            </p>
+          </div>
         </div>
       </div>
+      <!-- Quiz UI-->
+      <QuizUI :question="question" />
     </div>
-    <!-- Quiz UI-->
-    <QuizUI :question="data" />
-  </div>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
-//TODO: check local storage before making API call
-const { data } = await useFetch("/api/quiz");
+import { IShortQuestion, IStreak } from "~~/types/IStreak";
+
+const question = ref();
+
+let today = new Date();
+
+today.setHours(0, 0, 0, 0);
+
+const streaks = useLocalStorage<IStreak[]>("streaks", []);
+
+const fetchNStore = async () => {
+  console.log("Fetching");
+  const { data } = await useFetch("/api/quiz");
+  const streak: IStreak = {
+    date: today,
+    question: data.value as IShortQuestion,
+    completed: false,
+    passed: false,
+  };
+  streaks.value.push(streak);
+  question.value = streak.question;
+};
+
+if (streaks.value.length < 1) {
+  fetchNStore();
+} else {
+  const last_streak = streaks.value[0];
+  if (
+    new Date(last_streak.date).getMilliseconds() !== today.getMilliseconds()
+  ) {
+    fetchNStore();
+  } else {
+    question.value = last_streak.question;
+  }
+}
 </script>
---
+++ types/IStreak.ts
@@ -0,0 +1,19 @@
+import { Answers } from "~~/types/IAnswers";
+import { Tag } from "~~/types/ITag";
+
+export interface IStreak {
+  date: Date;
+  question: IShortQuestion;
+  completed: boolean;
+  passed: boolean;
+}
+
+export interface IShortQuestion {
+  id: number;
+  question: string;
+  answers: Answers;
+  tags: Tag[];
+  category: string;
+  difficulty: string;
+  correct_answer: string;
+}
