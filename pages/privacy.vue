41c70f199bd8ccf2a09b9a99daee82bd3f3f9b6c
kyng-cytro <johndibashi2001@gmail.com>
2022-11-30 23:48:21 +0100

    :sparkles: review page

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -2,5 +2,141 @@
   <Head>
     <Title>Code Streaks | Privacy policy</Title>
   </Head>
-  <div class="flex justify-center">Privacy</div>
+  <div class="flex justify-center">
+    <div class="flex flex-col items-center space-y-4">
+      <h3 class="text-2xl font-semibold">Privacy Policy</h3>
+      <div
+        class="scrollbar-hide flex max-h-[750px] max-w-xl flex-col space-y-2 overflow-y-auto overflow-x-hidden rounded-xl border-2 border-slate-400 bg-slate-300 py-10 px-6 dark:border-slate-400 dark:bg-slate-700"
+      >
+        <h3><strong> Policy for Code Streaks</strong></h3>
+        <div>
+          <p>
+            At Code Streaks, accessible from https://codestreaks.com, one of our
+            main priorities is the privacy of our visitors. This Privacy Policy
+            document contains types of information that is collected and
+            recorded by Code Streaks and how we use it.
+          </p>
+          <p>
+            If you have additional questions or require more information about
+            our Privacy Policy, do not hesitate to contact us.
+          </p>
+        </div>
+        <h3><strong>Log Files</strong></h3>
+        <div>
+          <p>
+            Code Streaks follows a standard procedure of using log files. These
+            files log visitors when they visit websites. All hosting companies
+            do this and a part of hosting services' analytics. The information
+            collected by log files include internet protocol (IP) addresses,
+            browser type, Internet Service Provider (ISP), date and time stamp,
+            referring/exit pages, and possibly the number of clicks. These are
+            not linked to any information that is personally identifiable. The
+            purpose of the information is for analyzing trends, administering
+            the site, tracking users' movement on the website, and gathering
+            demographic information.
+          </p>
+        </div>
+        <h3><strong>Google DoubleClick DART Cookie</strong></h3>
+        <div>
+          <p>
+            Google is one of a third-party vendor on our site. It also uses
+            cookies, known as DART cookies, to serve ads to our site visitors
+            based upon their visit to www.website.com and other sites on the
+            internet. However, visitors may choose to decline the use of DART
+            cookies by visiting the Google ad and content network Privacy Policy
+          </p>
+        </div>
+        <h3><strong>Our Advertising Partners</strong></h3>
+        <div>
+          <p>
+            Some of advertisers on our site may use cookies and web beacons. Our
+            advertising partners are listed below. Each of our advertising
+            partners has their own Privacy Policy for their policies on user
+            data.
+          </p>
+        </div>
+        <h3><strong> Policies</strong></h3>
+        <div>
+          <P
+            >You may consult this list to find the Privacy Policy for each of
+            the advertising partners of Code Streaks.</P
+          >
+          <p>
+            Third-party ad servers or ad networks uses technologies like
+            cookies, JavaScript, or Web Beacons that are used in their
+            respective advertisements and links that appear on Code Streaks,
+            which are sent directly to users' browser. They automatically
+            receive your IP address when this occurs. These technologies are
+            used to measure the effectiveness of their advertising campaigns
+            and/or to personalize the advertising content that you see on
+            websites that you visit.
+          </p>
+          <p>
+            Note that Code Streaks has no access to or control over these
+            cookies that are used by third-party advertisers.
+          </p>
+        </div>
+        <h3><strong>Third Party Privacy Policies</strong></h3>
+        <div>
+          <p>
+            Code Streaks's Privacy Policy does not apply to other advertisers or
+            websites. Thus, we are advising you to consult the respective
+            Privacy Policies of these third-party ad servers for more detailed
+            information. It may include their practices and instructions about
+            how to opt-out of certain options.
+          </p>
+          <p>
+            You can choose to disable cookies through your individual browser
+            options. To know more detailed information about cookie management
+            with specific web browsers, it can be found at the browsers'
+            respective websites.
+          </p>
+        </div>
+        <h3><strong>Children's Information</strong></h3>
+        <div>
+          <p>
+            Another part of our priority is adding protection for children while
+            using the internet. We encourage parents and guardians to observe,
+            participate in, and/or monitor and guide their online activity.
+          </p>
+
+          <p>
+            Code Streaks does not knowingly collect any Personal Identifiable
+            Information from children under the age of 13. If you think that
+            your child provided this kind of information on our website, we
+            strongly encourage you to contact us immediately and we will do our
+            best efforts to promptly remove such information from our records.
+          </p>
+        </div>
+        <h3><strong>Online Privacy Policy Only</strong></h3>
+        <div>
+          <p>
+            This Privacy Policy applies only to our online activities and is
+            valid for visitors to our website with regards to the information
+            that they shared and/or collect in Code Streaks. This policy is not
+            applicable to any information collected offline or via channels
+            other than this website.
+          </p>
+        </div>
+        <h3><strong>Consent</strong></h3>
+        <div>
+          <p>
+            By using our website, you hereby consent to our Privacy Policy and
+            agree to its Terms and Conditions.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<style scoped>
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
+
+/* For IE, Edge and Firefox */
+.scrollbar-hide {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+</style>
---
+++ pages/review/[id].vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="h-full">
+    <Head>
+      <Title>Code Streaks | {{ streak ? streak.date : "404" }}</Title>
+    </Head>
+    <ClientOnly>
+      <div v-if="streak">
+        <div class="flex justify-center">
+          <div
+            class="flex flex-col items-center justify-center space-y-10"
+            v-if="streak.question"
+          >
+            <!-- Page Header -->
+            <div class="flex flex-col items-center space-y-4">
+              <h1 class="text-2xl font-semibold">
+                {{ streak.date }}'s Challenge
+              </h1>
+              <div class="flex flex-col items-center space-y-2 text-sm">
+                <div class="flex gap-6">
+                  <p>Difficulty: {{ streak.question.difficulty }}</p>
+                  <p>Category: {{ streak.question.category }}</p>
+                </div>
+                <div>
+                  <p>
+                    Tags:
+                    <span class="ml-1" v-for="tag in streak.question.tags">{{
+                      tag.name
+                    }}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <!-- Question Review UI -->
+            <QuizUIReview :question="streak.question" />
+            <!-- Home Button-->
+            <NuxtLink
+              class="rounded-md border border-teal-400 px-5 py-1.5 transition-all hover:bg-teal-500 hover:text-slate-50 focus:bg-teal-700 focus:outline-none focus:ring-4 focus:ring-teal-300 dark:text-slate-100"
+              :to="`/`"
+            >
+              <span>Back Home</span>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <!-- 404 Page -->
+      <div
+        class="flex h-full flex-col items-center justify-center gap-10"
+        v-else
+      >
+        <div class="space-y-2">
+          <h3 class="text-center text-3xl">404</h3>
+          <p>Streak Not Found</p>
+        </div>
+        <NuxtLink
+          class="rounded-md border border-teal-400 px-5 py-1.5 transition-all hover:bg-teal-500 hover:text-slate-50 focus:bg-teal-700 focus:outline-none focus:ring-4 focus:ring-teal-300 dark:text-slate-100"
+          :to="`/`"
+        >
+          <span>Home</span>
+        </NuxtLink>
+      </div>
+      <div class="flex justify-center"></div>
+    </ClientOnly>
+  </div>
+</template>
+<script setup lang="ts">
+import { IStreak } from "~~/types/IStreak";
+
+const { id } = useRoute().params;
+
+const streak = ref<IStreak>();
+
+const streaks = useLocalStorage<IStreak[]>("streaks", []);
+
+streak.value = streaks.value.find((streak) => streak.id == Number(id));
+</script>
