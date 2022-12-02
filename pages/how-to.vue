87518bc895789bf44f51ea72fb470d12908725e4
kyng-cytro <johndibashi2001@gmail.com>
2022-12-02 21:09:33 +0100

    :sparkles: how to play

--- pages/how-to.vue
+++ pages/how-to.vue
@@ -2,5 +2,23 @@
   <Head>
     <Title>Code Streaks | How to play? </Title>
   </Head>
-  <div class="flex justify-center">How to Play</div>
+  <div class="flex justify-center">
+    <div class="flex flex-col items-center space-y-4">
+      <h3 class="text-2xl font-semibold">🚀 How to play?</h3>
+      <div
+        class="scrollbar-hide flex max-h-[750px] max-w-xl flex-col space-y-2 overflow-y-auto overflow-x-hidden rounded-xl border-2 border-slate-400 bg-slate-300 py-10 px-6 dark:border-slate-400 dark:bg-slate-700"
+      >
+        <h3 class="font-semibold">Play</h3>
+        <p>
+          Your task is to answer the random question of day. There is a new
+          challenge every day.
+        </p>
+        <h3 class="font-semibold">Share</h3>
+        <p>
+          You can also share your broken or holding streaks on Twitter or just
+          with your friends.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
