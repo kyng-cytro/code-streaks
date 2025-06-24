0eb9eed958ea5ef65862c938b7afb39fc9313704
kyng-cytro <johndibashi2001@gmail.com>
2025-06-24 16:48:45 +0100

    chore: one question per day

--- server/api/quiz.ts
+++ server/api/quiz.ts
@@ -1,3 +1,4 @@
+import moment from "moment";
 import type { IQuestion } from "@/types/IQuestion";
 
 function objForEach<T>(obj: T, f: (k: keyof T, v: T[keyof T]) => void): void {
@@ -8,29 +9,42 @@ function objForEach<T>(obj: T, f: (k: keyof T, v: T[keyof T]) => void): void {
   }
 }
 
-export default defineEventHandler(async () => {
-  const { quizApiToken } = useRuntimeConfig();
+export default defineCachedEventHandler(
+  async () => {
+    const { quizApiToken } = useRuntimeConfig();
 
-  const data: IQuestion[] = await $fetch(
-    `https://quizapi.io/api/v1/questions?apiKey=${quizApiToken}&limit=1`,
-  );
+    const data: IQuestion[] = await $fetch(
+      `https://quizapi.io/api/v1/questions?apiKey=${quizApiToken}&limit=1`,
+    );
 
-  objForEach(data[0].correct_answers, (k, v) => {
-    if (v === "true") {
-      data[0].correct_answer = k;
-    }
-  });
+    objForEach(data[0].correct_answers, (k, v) => {
+      if (v === "true") {
+        data[0].correct_answer = k;
+      }
+    });
 
-  const { id, question, answers, tags, category, difficulty, correct_answer } =
-    data[0];
+    const {
+      id,
+      question,
+      answers,
+      tags,
+      category,
+      difficulty,
+      correct_answer,
+    } = data[0];
 
-  return {
-    id,
-    question,
-    answers,
-    tags,
-    category,
-    difficulty,
-    correct_answer,
-  };
-});
+    return {
+      id,
+      question,
+      answers,
+      tags,
+      category,
+      difficulty,
+      correct_answer,
+    };
+  },
+  {
+    name: `quiz-${moment().format("YYYY-MM-DD")}`,
+    maxAge: moment().endOf("day").diff(moment(), "seconds"),
+  },
+);
